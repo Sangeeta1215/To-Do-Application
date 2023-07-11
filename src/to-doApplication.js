2917f6107d84f7a0c42bc168838b0909a4056d6d
Sangeeta Devi <sd705194@gmail.com>
2023-07-11 10:55:03 +0530

    initial commit

---
+++ src/to-doApplication.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Todo from "./todo.js";
+export default function ToDoApplication() {
+ 
+  return (
+    <>
+     <div className="container">
+       <div className="row">
+        <div className="col-4  border m-auto my-5">
+          <Todo />
+
+        </div>
+       </div>
+       </div>
+       </>
+   
+  )
+}
+
